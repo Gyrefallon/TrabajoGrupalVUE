0d00bfb205498e6a8134333e79a03e568a725645
Gyrefallon <gonzalo.2800@gmail.com>
2023-04-05 11:53:52 -0400

    Añadiendo todo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col text-center">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <p class="h3 display-3 text-muted">Vue Router</p>
+        <p class="h3 display-3 text-muted">Vue Home</p>
         <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aspernatur autem
           blanditiis consequuntur
           dolores, doloribus exercitationem, harum in ipsam iure mollitia saepe soluta tempora vel vero! Adipisci alias
