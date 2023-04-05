ea9a5a6ed2a41e132f62113494d9e6a335270d7e
Franchesca <franchesca.ramirezc@gmail.com>
2023-04-05 14:03:38 -0400

    agrego login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,33 @@
 <template>
-  <div class="container mt-3">
-    <div class="row">
-      <div class="col">
-        <p class="h3 fw-bold text-success">About Us</p>
-        <p class="fst-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias corporis exercitationem
-          iusto
-          maxime minima
-          nisi officia officiis quaerat quos rem! Adipisci facere minus perferendis ratione reiciendis saepe sint
-          suscipit temporibus.</p>
-      </div>
+    <div class="login">
+        <h1 class="title">Iniciar Sesión</h1>
+        <form action class="form" @submit.prevent="login">
+            <label class="form-label" for="#email">Email:</label>
+            <input v-model="email" class="form-input" type="email" id="email" required placeholder="Email">
+            <label class="form-label" for="#password">Password:</label>
+            <input v-model="password" class="form-input" type="password" id="password" placeholder="Password">
+            <p v-if="error" class="error">Has introducido mal el email o la contraseña.</p>
+            <input class="form-submit" type="submit" value="Login">
+        </form>
     </div>
-    <div class="row">
-      <div class="col">
-        <ul class="list-group">
-          <li class="list-group-item">
-            App Name : <span class="fw-bold">Vue Router</span>
-          </li>
-          <li class="list-group-item">
-            Autor : <span class="fw-bold">Reinaldo Blanco</span>
-          </li>
-          <li class="list-group-item">
-            App Version : <span class="fw-bold">1.0.1</span>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
 </template>
+
 <script>
+import { AuthLogin } from "@/services/AuthLogin";
 
 export default {
-  name: "About-Us",
-  components: {}
+    name: "LoginUser",
+    components: {},
+    data: function () {
+        return {
+            usuarios: AuthLogin.getAllUsers(),
+            email: "",
+            password: "",
+
+        };
+    }
 };
 </script>
+
+<style scoped></style>
+
