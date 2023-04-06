717b85cf2f4f7cf457a597c2920d2e0a5a1cbc42
Franchesca <franchesca.ramirezc@gmail.com>
2023-04-06 10:25:56 -0400

    agrego authlogin

---
+++ src/services/AuthLogin.js
@@ -0,0 +1,18 @@
+export class AuthLogin {
+    static usuarios = [
+        {
+        "id": 1,
+        "email": "fran@gmail.com",
+        "password": "123fran",
+        },
+        {
+        "id": 2,
+        "email": "betacor@gmail.com",
+        "password": "123beta",
+        }  
+    ];
+
+    static getAllUsuarios() {
+        return this.usuarios;
+    }
+}
