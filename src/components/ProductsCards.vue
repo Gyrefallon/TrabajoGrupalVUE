fd2d810330a6d3c07946ae7adeb846e26127861d
Manuel Alfaro <zignlash@live.cl>
2023-04-12 12:58:17 -0400

    Añade carro de compras basico (#1)

--- src/components/ProductsCards.vue
+++ src/components/ProductsCards.vue
@@ -1,21 +1,26 @@
 <template>
     <div class="container">
 			<div class="row">
-				<div v-for="fila in productos" :key="fila.nombre" class="card col-md-2" style="width: 18rem;">
+				<div v-for="fila in productos" :key="fila.id" class="card col-md-2" style="width: 18rem;">
 					<img :src= "fila.imagen" class="card-img-top" alt="placa">
 					<div class="card-body">
 							<h5 class="card-title">{{ fila.nombre }}</h5>
 							<h6 class="card-text">{{ fila.descripcion }}</h6>
-							<a href="#" class="btn btn-primary">Añadir al Carrito</a>
+							<h6 class="card-text">Precio: {{ fila.precio }}</h6>
+							<h6 class="card-text">Stock: {{ fila.stock }}</h6>
+							<a class="btn btn-primary" @click="anadirProducto(fila)">Añadir al Carrito</a>
 					</div>
 				</div>
 			</div>
     </div>
-	
+	<CarroCompra></CarroCompra>
 </template>
 
 <script>
-import { Products } from "@/services/Products.js";
+
+import {mapState, mapMutations} from 'vuex'
+import { Products } from "@/services/products.js";
+import CarroCompra from "@/components/CarroCompras.vue";
 
 export default {
 	name: "ProductsCards",
@@ -23,7 +28,17 @@ export default {
 		return {
 			productos: Products.getAllProducts()
 		};
-	}
+	},
+	components: {
+		CarroCompra
+	},
+	computed: {
+    ...mapState(['carro']),
+    ...mapState(['sumaTotal'])    
+     },
+     methods: {
+        ...mapMutations(['anadirProducto']),
+    },
 };
 </script>
   
